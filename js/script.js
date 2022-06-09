12c5ab83c85bb21a945391bb1a643f31881bd316
PieroPace <piero.pace88@gmail.com>
2022-06-10 00:07:31 +0200

    End

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Questo messaggio viene da VueJs',
+        myImage: 'https://picsum.photos/200/300'
+    }
+});
